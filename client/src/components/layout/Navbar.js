cf610ba94297df9f90ca02a0a1306c36db9a74da
nikhil-edisn <nikhilesh@edisn.ai>
2021-04-13 19:25:03 +0530

    React Router Setup

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
-    <h1>
-      <a href="index.html"><i className="fas fa-code"></i> DevConnector</a>
-    </h1>
-    <ul>
-      <li><a href="profiles.html">Developers</a></li>
-      <li><a href="register.html">Register</a></li>
-      <li><a href="login.html">Login</a></li>
-    </ul>
-  </nav>
-  );
-};
-
+      <h1>
+        <Link to="/" className="fas fa-code">
+          DevConnector
+        </Link>
+      </h1>
+      <ul>
+        <Link to="!#" className="fas fa-code">
+          Developers
+        </Link>
+        <Link to="/register" className="fas fa-code">
+          Register
+        </Link>
+        <Link to="/login" className="fas fa-code">
+          Login
+        </Link>
+      </ul>
+    </nav>
+  )
+}
 
 export default Navbar
